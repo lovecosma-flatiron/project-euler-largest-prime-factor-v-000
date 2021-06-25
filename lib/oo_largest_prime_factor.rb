cf65c3bfe2c436a1154c956e0fb29cf9745949cc
Matthew Williams <matthew.williams@flatironschool.com>
2021-06-24 21:05:10 -0600

    Done.

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,39 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    attr_accessor :input
+
+    def initialize input
+
+        @input = input
+
+    end 
+
+    def number
+
+        max = -1
+
+        while @input % 2 == 0
+            max = 2 
+            @input  = @input / 2
+        end 
+
+        counter = 3
+
+        while counter <= Math.sqrt(@input)
+
+            while @input % counter == 0
+
+                max = counter
+                @input = @input / counter 
+                
+            end 
+            counter += 2
+        end 
+
+        if @input > 2
+            max = @input 
+        end 
+        max
+    end 
+
+end 
